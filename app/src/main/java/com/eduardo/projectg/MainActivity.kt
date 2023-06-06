af0aed609e8b916e3111b32969a72ff39c2f4610
Eduardo Freire <61304055+EduardoGhost@users.noreply.github.com>
2023-06-06 19:25:52 -0300

    Screen Login Init

--- app/src/main/java/com/eduardo/projectg/MainActivity.kt
+++ app/src/main/java/com/eduardo/projectg/MainActivity.kt
@@ -6,12 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.eduardo.projectg.ui.theme.MenuDrawer
-import com.eduardo.projectg.ui.theme.ProjectGTheme
+import com.eduardo.projectg.ui.theme.ui.theme.ProjectGTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
