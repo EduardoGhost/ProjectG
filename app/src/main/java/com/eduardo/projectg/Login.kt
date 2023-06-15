cdeb8cb90ac5d89426106db24c42e2e2c43c67e3
Eduardo Freire <61304055+EduardoGhost@users.noreply.github.com>
2023-06-15 16:16:47 -0300

    update Login, strings.xml, build.gradle

--- app/src/main/java/com/eduardo/projectg/Login.kt
+++ app/src/main/java/com/eduardo/projectg/Login.kt
@@ -1,6 +1,5 @@
 package com.eduardo.projectg
 
-import android.media.AudioFocusRequest
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
@@ -9,14 +8,17 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
-
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
@@ -25,28 +27,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.eduardo.projectg.ui.theme.ui.theme.ProjectGTheme
 
-
-
 @Composable
 fun Login(){
+    val passwordFocusRequester = FocusRequester()
+    val focusManager: FocusManager = LocalFocusManager.current
+    val textFieldShapes = RoundedCornerShape(30.dp)
+
     Column(
         Modifier
             .padding(24.dp)
             .fillMaxSize(),
-        verticalArrangement = Arrangement.spacedBy(16.dp, alignment =
-        Alignment.Bottom),
+        verticalArrangement = Arrangement.spacedBy(16.dp, alignment = Alignment.Bottom),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 Icon(painter = painterResource(id = R.drawable.login_icon),
     null,
     Modifier.size(80.dp),
-    tint = Color.Blue
+    tint = Color.Blue,
 )
-        TextInput(InputType.Name)
-        TextInput(InputType.Password)
-        Button(onClick = {}, modifier = Modifier.size(100.dp))
+        TextInput(InputType.Name, keyboardActions = KeyboardActions(onNext = {passwordFocusRequester.requestFocus()}))
+        TextInput(InputType.Password, keyboardActions = KeyboardActions(onDone = {focusManager.clearFocus()}
+        ))
+        Button(onClick = {}, modifier = Modifier
+            .width(1000.dp)
+            .height(50.dp), shape = textFieldShapes)
         {
-            Text("Entrar", Modifier.padding(vertical = 8.dp))
+            Text(stringResource(R.string.sign_in), Modifier.padding(vertical = 8.dp))
         }
         Divider(color = Color.Black.copy(alpha = 0.3f),
         thickness = 1.dp,
@@ -54,9 +60,9 @@ Icon(painter = painterResource(id = R.drawable.login_icon),
 
             )
         Row(verticalAlignment = Alignment.CenterVertically) {
-            Text("Ainda não tem conta?", color = Color.Black)
+            Text(stringResource(R.string.dont_have_an_account_yet), color = Color.Black)
             TextButton(onClick = { /*TODO*/ }) {
-                Text("Escrever-se")
+                Text(stringResource(R.string.write_yourself))
             }
 
         }
@@ -87,20 +93,17 @@ sealed class InputType(
 
 }
 
-
-
-
 @Composable
 fun TextInput(inputType: InputType,
-//focusRequest: FocusRequester? = null,
-//        keyboardActions: KeyboardActions
+focusRequest: FocusRequester? = null,
+        keyboardActions: KeyboardActions
               ){
     var value by remember { mutableStateOf("") }
-    val textFieldShape = RoundedCornerShape(8.dp)
+    val textFieldShape = RoundedCornerShape(30.dp)
 
     TextField(value = value,
         onValueChange = {value = it},
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().focusRequester(focusRequest ?: FocusRequester()),
         leadingIcon = {Icon(imageVector = inputType.icon,null)},
         label = { Text(text = inputType.label)},
         shape = textFieldShape,
@@ -109,18 +112,12 @@ fun TextInput(inputType: InputType,
             disabledIndicatorColor = Color.Transparent,
             focusedIndicatorColor = Color.Transparent,
             unfocusedIndicatorColor = Color.Transparent
-        )
-        ,
-
-//        singleLine = true,
-//        keyboardActions = inputType.visualTransformation,
+        ),
+        singleLine = true,
+      //  keyboardActions = inputType.visualTransformation,
         visualTransformation = inputType.visualTransformation,
         keyboardOptions = inputType.keyboardOptions
-
-
     )
-
-
 }
 
 @Preview(showBackground = true)
